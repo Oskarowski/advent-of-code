6cdc8c1c204852a711bb0fd7c80112209b1c903a
Oskarowski <oscar.kacprzak@gmail.com>
2024-04-20 20:18:42 +0000

    done part 2 - Cache is King

--- day_12/solution.ts
+++ day_12/solution.ts
@@ -6,165 +6,181 @@ enum SpringState {
     UNKNOWN = '?',
 }
 
-class ConditionRecord {
-    row: string;
-    providedGroups: number[];
-    validRecordCombinations: string[] = [];
-    countValidRecordCombinations: number = 0;
-
-    constructor(row: string, providedGroups: number[]) {
-        this.row = row;
-        this.providedGroups = providedGroups;
-    }
-
-    addValidRecordCombination(combination: string) {
-        this.validRecordCombinations.push(combination);
-        this.countValidRecordCombinations++;
-    }
-}
-
 class SolutionDay12 {
     puzzleInput: string[];
-    parsedPuzzleInput: ConditionRecord[];
 
     setPuzzleInput(input: string[]) {
         this.puzzleInput = input;
     }
 
-    parsePuzzleInput() {
-        this.parsedPuzzleInput = [];
+    sumSpringRecordCombinations(springConditionRecords: string, unfold: boolean = false): number {
+        let totalCombinations = 0;
+        const records = springConditionRecords.split('\n');
 
-        for (let i = 0; i < this.puzzleInput.length; i++) {
-            const row = this.puzzleInput[i];
+        for (const springRecord of records) {
+            const [springState, damagedSpringRecord] = springRecord.split(' ');
+            const damagedSpringRecordArray = damagedSpringRecord
+                .split(',')
+                .map(Number);
 
-            const [springsString, providedGroupsString] = row.split(' ');
-            const providedGroups = providedGroupsString.split(',').map(Number);
-
-            const conditionRecord = new ConditionRecord(
-                springsString,
-                providedGroups
-            );
-
-            this.parsedPuzzleInput.push(conditionRecord);
-        }
-    }
-
-    generateCombinationsAndCount() {
-        const areArraysEqual = (arr1: number[], arr2: number[]) => {
-            if (arr1.length !== arr2.length) {
-                return false;
+            if (unfold) {
+                // Unfold spring state and damaged spring record
+                const unfoldedSpringState = Array(5).fill(springState).join('?');
+                const unfoldedDamagedSpringRecord = Array(5)
+                    .fill(damagedSpringRecordArray)
+                    .flat();
+    
+                totalCombinations += this.getValidSpringRecordCombinations(
+                    unfoldedSpringState,
+                    unfoldedDamagedSpringRecord
+                );
+            }
+            else {
+                totalCombinations += this.getValidSpringRecordCombinations(
+                    springState,
+                    damagedSpringRecordArray
+                );
             }
 
-            return arr1.every((value, index) => value === arr2[index]);
-        };
-
-        for (const record of this.parsedPuzzleInput) {
-            const combinations = this.generateSpringRecordCombinations(
-                record.row
-            );
-
-            // console.log('Record:', record.row);
-
-            for (const combination of combinations) {
-                // console.log('Combination:', combination);
-
-                const countedConDamagedSprings =
-                    this.countContinuousDamagedSprings(combination);
-
-                if (
-                    areArraysEqual(
-                        record.providedGroups,
-                        countedConDamagedSprings
-                    )
-                ) {
-                    // console.log('\n Valid combination:', combination, `\n`);
-                    record.addValidRecordCombination(combination);
-                }
-            }
-
-            // console.log(
-            //     'Valid combinations count:',
-            //     record.countValidRecordCombinations,
-            //     '\n'
-            // );
         }
+
+        return totalCombinations;
     }
 
-    countContinuousDamagedSprings(line: string): number[] {
-        const counts: number[] = [];
-        let continuousCount = 0;
+    cache: Map<string, number> = new Map();
 
-        for (const char of line) {
-            if (char === '#') {
-                continuousCount++;
+    getValidSpringRecordCombinations(
+        springState: string,
+        damagedSpringRecord: number[]
+    ): number {
+        const key = springState + '|' + damagedSpringRecord.join(',');
+
+        // Check if the result is already cached
+        if (this.cache.has(key)) {
+            return this.cache.get(key)!;
+        }
+
+        if (damagedSpringRecord.length === 0) {
+            if (springState.includes(SpringState.DAMAGED)) {
+                return 0;
             } else {
-                if (continuousCount > 0) {
-                    counts.push(continuousCount);
-                    continuousCount = 0;
-                }
+                return 1;
             }
         }
 
-        if (continuousCount > 0) {
-            counts.push(continuousCount);
+        if (springState.length === 0) {
+            if (damagedSpringRecord.length === 0) {
+                return 1;
+            } else {
+                return 0;
+            }
         }
 
-        return counts;
-    }
+        let totalCombinations = 0;
 
-    generateSpringRecordCombinations(
-        springRecord: string,
-        index: number = 0
-    ): string[] {
-        if (index >= springRecord.length) {
-            return [springRecord];
-        }
-
-        if (springRecord[index] === SpringState.UNKNOWN) {
-            // replace '?' with '.' and combine results
-            const withOperational = this.generateSpringRecordCombinations(
-                springRecord.substring(0, index) +
-                    SpringState.OPERATIONAL +
-                    springRecord.substring(index + 1),
-                index + 1
-            );
-            // replace '?' with '#' and combine results
-            const withDamaged = this.generateSpringRecordCombinations(
-                springRecord.substring(0, index) +
-                    SpringState.DAMAGED +
-                    springRecord.substring(index + 1),
-                index + 1
-            );
-            return withOperational.concat(withDamaged);
-        } else {
-            // continue recursion for next index
-            return this.generateSpringRecordCombinations(
-                springRecord,
-                index + 1
+        // if "." or "?"
+        if (
+            springState[0] === SpringState.OPERATIONAL ||
+            springState[0] === SpringState.UNKNOWN
+        ) {
+            totalCombinations += this.getValidSpringRecordCombinations(
+                springState.substring(1),
+                damagedSpringRecord
             );
         }
-    }
 
-    sumAllValidRecordCombinations(): number {
-        return this.parsedPuzzleInput.reduce((acc, record) => {
-            return acc + record.countValidRecordCombinations;
-        }, 0);
-    }
-
-    printParsedPuzzleInput() {
-        for (const record of this.parsedPuzzleInput) {
-            console.log(record);
+        // if "#" or "?"
+        if (
+            springState[0] === SpringState.DAMAGED ||
+            springState[0] === SpringState.UNKNOWN
+        ) {
+            if (this.isValidCondition(springState, damagedSpringRecord)) {
+                totalCombinations += this.getValidSpringRecordCombinations(
+                    springState.substring(damagedSpringRecord[0] + 1),
+                    damagedSpringRecord.slice(1)
+                );
+            }
         }
+
+        // Cache the result
+        this.cache.set(key, totalCombinations);
+
+        return totalCombinations;
+    }
+
+    isValidCondition(
+        springState: string,
+        damagedSpringRecord: number[]
+    ): boolean {
+        return (
+            damagedSpringRecord[0] <= springState.length &&
+            !springState
+                .substring(0, damagedSpringRecord[0])
+                .includes(SpringState.OPERATIONAL) &&
+            (damagedSpringRecord[0] === springState.length ||
+                springState[damagedSpringRecord[0]] !== SpringState.DAMAGED)
+        );
+    }
+}
+
+
+const processTests = () => {
+    console.log('T E S T S')
+
+    const solver1 = new SolutionDay12();
+    const solvedPuzzle1 = getPuzzleInput('day_12_input').reduce(
+        (acc, record) =>
+            (acc += solver1.sumSpringRecordCombinations(record, true)),
+        0
+    )
+    if(solvedPuzzle1 === 1738259948652){
+        console.log(`✅ Test passed for day_12_input output: ${solvedPuzzle1}`);
+    }else{
+        console.log(`❌ Test failed for day_12_input expected output to be 1738259948652 but got ${solvedPuzzle1}`);
+    }
+
+    const solver2 = new SolutionDay12();
+    const solvedPuzzle2 = getPuzzleInput('day_12_t2_input').reduce(
+        (acc, record) =>
+            (acc += solver2.sumSpringRecordCombinations(record, true)),
+        0
+    )
+    if(solvedPuzzle2 === 525152){
+        console.log(`✅ Test passed for day_12_t2_input output: ${solvedPuzzle2}`);
+    }else{
+        console.log(`❌ Test failed for day_12_t2_input expected output to be 525152 but got ${solvedPuzzle2}`);
+    }
+
+    const solver3 = new SolutionDay12();
+    const solvedPuzzle3 = getPuzzleInput('day_12_t2_input').reduce(
+        (acc, record) =>
+            (acc += solver3.sumSpringRecordCombinations(record)),
+        0
+    )
+    if(solvedPuzzle3 === 21){
+        console.log(`✅ Test passed for day_12_t2_input output: ${solvedPuzzle3}`);
+    }else{
+        console.log(`❌ Test failed for day_12_t2_input expected output to be 21 but got ${solvedPuzzle3}`);
     }
 }
 
 (() => {
-    console.log('Day 12');
+    console.log('------------------- Day 12 -------------------');
+    console.time('How much time to process Puzzle');
     const solution = new SolutionDay12();
-    solution.setPuzzleInput(getPuzzleInput('day_12_input'));
-    solution.parsePuzzleInput();
-    solution.generateCombinationsAndCount();
-    // solution.printParsedPuzzleInput();
+    console.time('How much time to process combination');
+    console.log('The sum of all possible combinations is:',
+        getPuzzleInput('day_12_input').reduce(
+            (acc, record) =>
+                (acc += solution.sumSpringRecordCombinations(record, true)),
+            0
+        )
+    );
 
-    console.log("The sum of all possible combinations is:", solution.sumAllValidRecordCombinations())
+    console.timeEnd('How much time to process combination');
+    // solution.printParsedPuzzleInput();
+    console.timeEnd('How much time to process Puzzle');
+
+    processTests();
 })();
+
