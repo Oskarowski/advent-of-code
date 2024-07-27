8b5dcb4f8c7f1ea770f0599fd9316e13554e595e
Oskarowski <oscar.kacprzak@gmail.com>
2024-07-27 08:51:08 +0000

    mis

--- helpers/getPuzzleInput.js
+++ helpers/getPuzzleInput.js
@@ -1,11 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPuzzleInput = void 0;
-const fs = require('fs');
-const getPuzzleInput = (fileName) => fs
-    .readFileSync(`${__dirname}/../data/${fileName}.txt`)
-    .toString()
-    .replace(/\r/g, '')
-    .trim()
-    .split('\n');
+var fs = require('fs');
+var getPuzzleInput = function (fileName) {
+    return fs
+        .readFileSync("".concat(__dirname, "/../data/").concat(fileName, ".txt"))
+        .toString()
+        .replace(/\r/g, '')
+        .trim()
+        .split('\n');
+};
 exports.getPuzzleInput = getPuzzleInput;
