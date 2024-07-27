bc7bbeec442f0b49ff42f510f477981e1b7c15a5
Oskarowski <oscar.kacprzak@gmail.com>
2024-07-27 13:00:21 +0000

    printDayDirectoriesByYear

--- cli.ts
+++ cli.ts
@@ -1,43 +1,32 @@
 import inquirer from 'inquirer';
-import {
-    addNewDay,
-    chooseExistingDay,
-    provideInputPath,
-    pasteInput,
-    printHello,
-} from './handlers.js';
+import { printDayDirectoriesByYear } from './handlers.js';
 
 async function main() {
-    const answers = await inquirer.prompt([
+    const actionAnswer = await inquirer.prompt([
         {
             type: 'list',
             name: 'action',
             message: 'What would you like to do?',
-            choices: [
-                'Add new day',
-                'Choose existing day',
-                'Provide path to puzzle input',
-                'Paste the input',
-                'Print Hello World',
-            ],
+            choices: ['List all days with solutions'],
         },
     ]);
 
-    switch (answers.action) {
-        case 'Add new day':
-            await addNewDay();
-            break;
-        case 'Choose existing day':
-            await chooseExistingDay();
-            break;
-        case 'Provide path to puzzle input':
-            await provideInputPath();
-            break;
-        case 'Paste the input':
-            await pasteInput();
-            break;
-        case 'Print Hello World':
-            await printHello();
+    const yearAnswer = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'year',
+            message: 'Enter the year:',
+            validate: (input) => {
+                return /^\d{4}$/.test(input)
+                    ? true
+                    : 'Please enter a valid year.';
+            },
+        },
+    ]);
+
+    switch (actionAnswer.action) {
+        case 'List all days with solutions':
+            await printDayDirectoriesByYear(yearAnswer.year);
             break;
     }
 }
--- handlers.ts
+++ handlers.ts
@@ -1,138 +1,59 @@
 import inquirer from 'inquirer';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
 import fs from 'node:fs';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import chalk from 'chalk';
 
 // Derive __dirname from import.meta.url
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export async function addNewDay() {
-    const { day } = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'day',
-            message: 'Enter the day number:',
-        },
-    ]);
+/**
+ * Function to list all day directories for a specific year
+ *
+ * @export
+ * @param {string} chosenYear
+ * @return {Promise<void>}  {Promise<void>}
+ */
+export async function printDayDirectoriesByYear(
+    chosenYear: string
+): Promise<void> {
+    const solutionsYearDir = path.join(__dirname, chosenYear);
 
-    const dayDir = join(__dirname, 'days', `day${day}`);
-    if (!fs.existsSync(dayDir)) {
-        fs.mkdirSync(dayDir, { recursive: true });
-    }
-
-    fs.writeFileSync(join(dayDir, 'input.txt'), '');
-    fs.writeFileSync(
-        join(dayDir, 'solution.ts'),
-        `
-    export function solve(input: string) {
-      // Your solution here
-    }
-  `
-    );
-
-    console.log(`Day ${day} created successfully.`);
-}
-
-export async function chooseExistingDay() {
-    const days = fs
-        .readdirSync(join(__dirname, 'days'))
-        .filter((dir: string) => dir.startsWith('day'));
-
-    const { day } = await inquirer.prompt([
-        {
-            type: 'list',
-            name: 'day',
-            message: 'Choose a day:',
-            choices: days,
-        },
-    ]);
-
-    const input = fs.readFileSync(
-        join(__dirname, 'days', day, 'input.txt'),
-        'utf-8'
-    );
-
-    // Import the solve function dynamically
-    const { solve } = await import(
-        `file://${join(__dirname, 'days', day, 'solution.ts')}`
-    );
-
-    const startTime = Date.now();
-    const result = solve(input);
-    const endTime = Date.now();
-
-    console.log(`Result: ${result}`);
-    console.log(`Execution time: ${endTime - startTime}ms`);
-}
-
-export async function provideInputPath() {
-    const { filePath } = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'filePath',
-            message: 'Provide the path to the puzzle input file:',
-        },
-    ]);
-
-    const input = fs.readFileSync(filePath, 'utf-8');
-    const { day } = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'day',
-            message: 'Enter the day number for which to use this input:',
-        },
-    ]);
-
-    const dayDir = join(__dirname, 'days', `day${day}`);
-    if (!fs.existsSync(dayDir)) {
-        console.error(`Day ${day} does not exist.`);
+    if (!fs.existsSync(solutionsYearDir)) {
+        console.log(
+            chalk.red(
+                `The directory for the year ${chosenYear} does not exist.`
+            )
+        );
         return;
     }
 
-    const { solve } = await import(`file://${join(dayDir, 'solution.ts')}`);
+    const allDirentInDir = fs.readdirSync(solutionsYearDir, {
+        withFileTypes: true,
+    });
 
-    const startTime = Date.now();
-    const result = solve(input);
-    const endTime = Date.now();
+    const daySolutionsDirs = allDirentInDir
+        .filter(
+            (dirent) => dirent.isDirectory() && dirent.name.startsWith('day_')
+        )
+        .map((dirent) => dirent.name);
 
-    console.log(`Result: ${result}`);
-    console.log(`Execution time: ${endTime - startTime}ms`);
-}
-
-export async function pasteInput() {
-    const { input } = await inquirer.prompt([
-        {
-            type: 'editor',
-            name: 'input',
-            message: 'Paste your puzzle input:',
-        },
-    ]);
-
-    const { day } = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'day',
-            message: 'Enter the day number for which to use this input:',
-        },
-    ]);
-
-    const dayDir = join(__dirname, 'days', `day${day}`);
-    if (!fs.existsSync(dayDir)) {
-        console.error(`Day ${day} does not exist.`);
-        return;
+    if (daySolutionsDirs.length > 0) {
+        console.log(
+            chalk.green(
+                `Days with solutions for year ${chalk.bold(chosenYear)}:`
+            )
+        );
+        daySolutionsDirs.forEach((dir) => {
+            const [dayPart, ...rest] = dir.split('_');
+            const formattedDayPart = chalk.cyan(dayPart + '_'); // Color day
+            const formattedDay = chalk.yellow(rest.join('_')); // Color which day
+            console.log(`- ${formattedDayPart}${formattedDay}`);
+        });
+    } else {
+        console.log(
+            chalk.red(`No days with solutions found for year ${chosenYear}.`)
+        );
     }
-
-    const { solve } = await import(`file://${join(dayDir, 'solution.ts')}`);
-
-    const startTime = Date.now();
-    const result = solve(input);
-    const endTime = Date.now();
-
-    console.log(`Result: ${result}`);
-    console.log(`Execution time: ${endTime - startTime}ms`);
-}
-
-export async function printHello(){
-    console.log("Hi, Hello World");
 }
