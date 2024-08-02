52864a5d4cfe4694ac37b32be346f3e898e00dcf
Oskarowski <oscar.kacprzak@gmail.com>
2024-08-02 19:11:21 +0000

    add main function, as entry file for now

---
+++ 2023/main.ts
@@ -0,0 +1,9 @@
+import { GenericPuzzleTester } from './GenericPuzzleTester.js';
+import path from 'path';
+
+(async () => {
+    const tester = new GenericPuzzleTester();
+    const testCasesPath = path.resolve('2023', 'data', 'test_cases.json');
+    const testsResults = await tester.runTests([14], testCasesPath);
+    await GenericPuzzleTester.displayTestResults(testsResults);
+})();
