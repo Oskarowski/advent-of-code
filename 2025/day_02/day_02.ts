bdece75051abc9f2417798e4fee78b29a094b778
Oskarowski <oscar.kacprzak@gmail.com>
2025-12-16 15:17:24 +0100

    use .reduce

--- 2025/day_02/day_02.ts
+++ 2025/day_02/day_02.ts
@@ -3,8 +3,6 @@ const puzzleContent = await Bun.file('./t.txt')
     .then((data) => data.trim().replace(/\n/g, ''));
 
 function solvePart1(puzzleContent: string): number {
-    const IDRangesStr = puzzleContent.split(',').map((r) => r.trim());
-
     const countDigits = (n: number): number => {
         if (n === 0) return 1;
         return Math.floor(Math.log10(Math.abs(n))) + 1;
@@ -24,18 +22,18 @@ function solvePart1(puzzleContent: string): number {
         return leftHalf === rightHalf;
     };
 
-    let sum = 0;
+    const summedInvalidIDs = puzzleContent
+        .split(',')
+        .map((r) => r.trim())
+        .reduce((acc: number, curr: string, index: number) => {
+            const [start, end] = curr.split('-').map(Number);
+            for (let id = start; id <= end; id++) {
+                if (isInvalidID(id)) acc += id;
+            }
+            return acc;
+        }, 0);
 
-    for (const IDRangeStr of IDRangesStr) {
-        const [start, end] = IDRangeStr.split('-').map(Number);
-        const size = end - start + 1;
-
-        for (let id = start; id <= end; id++) {
-            if (isInvalidID(id)) sum += id;
-        }
-    }
-
-    return sum;
+    return summedInvalidIDs;
 }
 
 console.log(`Part 1 solution: ${solvePart1(puzzleContent)}`);
@@ -76,17 +74,18 @@ function solvePart2(puzzleContent: string): number {
         return false;
     };
 
-    let sum = 0;
+    const summedInvalidIDs = puzzleContent
+        .split(',')
+        .map((r) => r.trim())
+        .reduce((acc: number, curr: string, index: number) => {
+            const [start, end] = curr.split('-').map(Number);
+            for (let id = start; id <= end; id++) {
+                if (isInvalidID(id)) acc += id;
+            }
+            return acc;
+        }, 0);
 
-    for (const IDRangeStr of IDRangesStr) {
-        const [start, end] = IDRangeStr.split('-').map(Number);
-
-        for (let id = start; id <= end; id++) {
-            if (isInvalidID(id)) sum += id;
-        }
-    }
-
-    return sum;
+    return summedInvalidIDs;
 }
 
 console.log(`Part 2 solution: ${solvePart2(puzzleContent)}`);
