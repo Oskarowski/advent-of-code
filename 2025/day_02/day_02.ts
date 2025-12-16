7909fcbfbc10b59bb62d687336f1b7fbbb79586f
Oskarowski <oscar.kacprzak@gmail.com>
2025-12-16 15:06:49 +0100

    done day 2 part 2 2025

--- 2025/day_02/day_02.ts
+++ 2025/day_02/day_02.ts
@@ -39,3 +39,54 @@ function solvePart1(puzzleContent: string): number {
 }
 
 console.log(`Part 1 solution: ${solvePart1(puzzleContent)}`);
+
+function solvePart2(puzzleContent: string): number {
+    const IDRangesStr = puzzleContent.split(',').map((r) => r.trim());
+
+    const countDigits = (n: number): number => {
+        if (n === 0) return 1;
+        return Math.floor(Math.log10(Math.abs(n))) + 1;
+    };
+
+    const isInvalidID = (num: number): boolean => {
+        const digitCount = countDigits(num);
+
+        for (let p = 1; p <= Math.floor(digitCount / 2); p++) {
+            if (digitCount % p !== 0) continue;
+
+            const repetitions = digitCount / p;
+            const divisor = Math.pow(10, p);
+
+            let temp = num;
+            const firstLeftChunk = num % divisor;
+
+            let allMatch = true;
+            for (let i = 0; i < repetitions; i++) {
+                const chunk = temp % divisor;
+                if (chunk !== firstLeftChunk) {
+                    allMatch = false;
+                    break;
+                }
+                temp = Math.floor(temp / divisor);
+            }
+
+            if (allMatch) return true;
+        }
+
+        return false;
+    };
+
+    let sum = 0;
+
+    for (const IDRangeStr of IDRangesStr) {
+        const [start, end] = IDRangeStr.split('-').map(Number);
+
+        for (let id = start; id <= end; id++) {
+            if (isInvalidID(id)) sum += id;
+        }
+    }
+
+    return sum;
+}
+
+console.log(`Part 2 solution: ${solvePart2(puzzleContent)}`);
