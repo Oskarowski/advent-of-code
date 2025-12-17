6f46720f16ac2448c5bac69d614076ceb6d73b42
Oskarowski <oscar.kacprzak@gmail.com>
2025-12-17 11:57:21 +0100

    done day 2 part 2 2025

--- 2025/day_03/day_03.ts
+++ 2025/day_03/day_03.ts
@@ -18,7 +18,6 @@ function solvePart1(puzzleContent: string): number {
                     }
                 }
             }
-            // console.log('For', curr, 'max joltages found:', max);
 
             return acc + max;
         }, 0);
@@ -26,4 +25,39 @@ function solvePart1(puzzleContent: string): number {
     return maxTotalJoltage;
 }
 
-console.log(`Part 1 solution: ${solvePart1(puzzleContent)}`);
+console.log('Part 1 solution:', solvePart1(puzzleContent));
+
+function solvePart2(puzzleContent: string): number {
+    const lines = puzzleContent.split('\n').map((r) => r.trim());
+
+    const findMaxKDigits = (line: string, k = 12): number => {
+        const lineLength = line.length;
+        const result: string[] = [];
+        let srcIndex = 0;
+
+        for (let i = 0; i < k; i++) {
+            const maxSearchIndex = lineLength - k + i;
+            let maxDigit = '-1';
+            let maxDigitIndex = srcIndex;
+
+            for (let j = srcIndex; j <= maxSearchIndex; j++) {
+                if (line[j] > maxDigit) {
+                    maxDigit = line[j];
+                    maxDigitIndex = j;
+                }
+            }
+            result.push(maxDigit);
+            srcIndex = maxDigitIndex + 1;
+        }
+        return Number(result.join(''));
+    };
+
+    const totalMaxJoltage = lines.reduce((acc: number, curr: string) => {
+        const maxLineJoltage = findMaxKDigits(curr, 12);
+        return acc + maxLineJoltage;
+    }, 0);
+
+    return totalMaxJoltage;
+}
+
+console.log('Part 2 solution:', solvePart2(puzzleContent));
