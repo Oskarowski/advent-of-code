bc9e3cb17106f8e2b1030acf9e9e6fb3ffd86493
Oskarowski <oscar.kacprzak@gmail.com>
2026-02-05 15:28:09 +0100

    done part 2 day 07 2025

--- 2025/day_07/day_07.ts
+++ 2025/day_07/day_07.ts
@@ -1,41 +1,34 @@
-const puzzleContent = await Bun.file('./t.txt')
+const puzzleContent = await Bun.file('./p.txt')
     .text()
     .then((d) => d.trim());
 
-const transformIntoMatrix = (puzzleContent: string) => puzzleContent.split('\n').map((line) => line.trim());
-
 function solvePart1(puzzleContent: string): number {
-    let puzzleMatrix = transformIntoMatrix(puzzleContent);
+    let puzzleMatrix = puzzleContent.split('\n').map((line) => line.trim().split(''));
     let totalSplits = 0;
 
     for (let y = 0; y < puzzleMatrix.length - 1; y++) {
         const currentLine = puzzleMatrix[y];
         const ny = y + 1;
-        let nextLine = puzzleMatrix[ny].split('');
+        let nextLine = puzzleMatrix[ny];
 
         for (let cx = 0; cx < currentLine.length; cx++) {
-            if (currentLine[cx] === '|' || currentLine[cx] === 'S') {
-                const downChar = nextLine[cx];
-                if (downChar === '.') {
-                    nextLine[cx] = '|';
-                } else if (downChar === '^') {
-                    const nxl = cx - 1;
-                    const nxr = cx + 1;
-                    try {
-                        if (nextLine[nxl] === '.') {
-                            nextLine[nxl] = '|';
-                        }
-                        if (nextLine[nxr] === '.') {
-                            nextLine[nxr] = '|';
-                        }
-                        totalSplits++;
-                    } catch (e) {
-                        // ignore out of bounce
-                    }
+            if (!(currentLine[cx] === '|' || currentLine[cx] === 'S')) continue;
+
+            const downChar = nextLine[cx];
+            if (downChar === '.') {
+                nextLine[cx] = '|';
+            } else if (downChar === '^') {
+                const nxl = cx - 1;
+                const nxr = cx + 1;
+                if (nxl >= 0 && nextLine[nxl] === '.') {
+                    nextLine[nxl] = '|';
                 }
+                if (nxr < nextLine.length && nextLine[nxr] === '.') {
+                    nextLine[nxr] = '|';
+                }
+                totalSplits++;
             }
         }
-        puzzleMatrix[ny] = nextLine.join('');
     }
 
     return totalSplits;
@@ -44,7 +37,46 @@ function solvePart1(puzzleContent: string): number {
 console.log('Part 1 solution:', solvePart1(puzzleContent));
 
 function solvePart2(puzzleContent: string): number {
-    return -1;
+    const puzzleMatrix = puzzleContent.split('\n').map((line) => line.trim().split(''));
+    const timelinesMatrix = Array.from({ length: puzzleMatrix.length }, () =>
+        Array.from({ length: puzzleMatrix[0].length }, () => 0)
+    );
+
+    for (let y = 0; y < puzzleMatrix[0].length; y++) {
+        if (puzzleMatrix[0][y] === 'S') {
+            timelinesMatrix[0][y] = 1;
+            break;
+        }
+    }
+
+    for (let y = 0; y < timelinesMatrix.length - 1; y++) {
+        const timelineRow = timelinesMatrix[y];
+        const dy = y + 1;
+        const nextRow = puzzleMatrix[dy];
+
+        for (let x = 0; x < timelineRow.length; x++) {
+            const point = timelineRow[x];
+            if (point === 0) continue;
+
+            const downstreamChar = nextRow[x];
+            if (downstreamChar === '.') {
+                timelinesMatrix[dy][x] += timelinesMatrix[y][x];
+                continue;
+            }
+            if (downstreamChar === '^') {
+                const nxl = x - 1;
+                const nxr = x + 1;
+                if (nxl >= 0 && nextRow[nxl] === '.') {
+                    timelinesMatrix[dy][nxl] += timelinesMatrix[y][x];
+                }
+                if (nxr < nextRow.length && nextRow[nxr] === '.') {
+                    timelinesMatrix[dy][nxr] += timelinesMatrix[y][x];
+                }
+            }
+        }
+    }
+
+    return timelinesMatrix.at(-1).reduce((acc, curr) => acc + curr, 0);
 }
 
-// console.log('Part 2 solution:', solvePart2(puzzleContent));
+console.log('Part 2 solution:', solvePart2(puzzleContent));
