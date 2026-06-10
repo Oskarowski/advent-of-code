847f0f76a2b1e5219c3ea30de6482bd5678e62cf
Oskarowski <oscar.kacprzak@gmail.com>
2026-06-10 19:29:56 +0200

    done day 08 / 2019 part 2

--- 2019/day_08/day_08.go
+++ 2019/day_08/day_08.go
@@ -8,21 +8,23 @@ import (
 	"runtime"
 )
 
+const (
+	LAYER_HORIZONTAL_SIZE = 25
+	LAYER_VERTICAL_SIZE   = 6
+)
+
 func main() {
 	fmt.Println("\nAoC - 08 / 2019")
 
 	_, file, _, _ := runtime.Caller(0)
-	dir := filepath.Dir(file)
 
-	fileData, err := os.ReadFile(path.Join(dir, "p.txt"))
+	fileData, err := os.ReadFile(path.Join(filepath.Dir(file), "p.txt"))
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return
 	}
 
-	layerHorizontalSize := 25
-	layerVerticalSize := 6
-	layerSize := layerHorizontalSize * layerVerticalSize
+	layerSize := LAYER_HORIZONTAL_SIZE * LAYER_VERTICAL_SIZE
 	layerCount := len(fileData) / layerSize
 
 	layers := make([]string, layerCount)
@@ -55,4 +57,34 @@ func main() {
 
 	imageCheckSum := computeCharInstances(layers[fewestZerosLayerIndex], '1') * computeCharInstances(layers[fewestZerosLayerIndex], '2')
 	fmt.Printf("\nPart 1 Image Checksum: %d", imageCheckSum)
+
+	finalImage := make([]rune, layerSize)
+
+	for idx := range layerSize {
+		finalPixel := '2'
+
+		for _, layer := range layers {
+			if layer[idx] != '2' {
+				finalPixel = rune(layer[idx])
+				break
+			}
+		}
+		finalImage[idx] = finalPixel
+	}
+
+	fmt.Println("\nPart 2:")
+
+	for y := range LAYER_VERTICAL_SIZE {
+		for x := range LAYER_HORIZONTAL_SIZE {
+			pixel := finalImage[y*LAYER_HORIZONTAL_SIZE+x]
+
+			if pixel == '1' {
+				fmt.Print("█")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+
 }
